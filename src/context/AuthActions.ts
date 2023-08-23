f2789ce5d9bf8e88e58ffa8a5305050573b4e930
shreeshakhadka <khadkashreesha08@gmail.com>
2023-08-23 18:54:56 -0400

    commit

---
+++ src/context/AuthActions.ts
@@ -0,0 +1,11 @@
+export const LoginStart = (user) =>({
+    type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user) =>({
+    type: "LOGIN_SUCCESS",
+});
+
+export const LoginFailure = (user) =>({
+    type: "LOGIN_FAILURE",
+});
---
+++ src/context/AuthContext.ts
@@ -0,0 +1,30 @@
+// import {createContext, useReducer} from "react";
+
+
+// const INITIAL_STATE ={
+//     user:null,
+//     isFetching: false,
+//     error: false
+// };
+
+
+// export const AuthContext = createContext(INITIAL_STATE);
+
+// export const AuthContextProvider =({children}) =>{
+//     const[state, dispatch] = useReducer(AuthContext, INITIAL_STATE);
+
+//     return(
+//         <AuthContext.Provider
+//         value={{
+//             user:state.user,
+//             isFetching: state.isFetching,
+//             error:state.error,
+
+//            }}
+//         >
+        
+//             {children}
+//         </AuthContext.Provider>
+
+//     )
+// }
