3e9ff028281e539dd2b9c74031b6b7a723ff0e97
shreeshakhadka <khadkashreesha08@gmail.com>
2023-08-23 16:05:35 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import HomePage from "./pages/home/HomePage"
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import HomePage from "./pages/home/HomePage"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Profile from "./pages/profile/Profile"
+import {BrowserRouter , Routes, Route} from "react-router-dom"
+
 
 function App() {
-  return <HomePage/>
+  return(
+    <BrowserRouter>
+      <Routes>
+      <Route path = "/"> element={<HomePage/>} </Route>
+
+        <Route path = "/login">
+          <Login/>
+        </Route>
+
+        <Route path = "register">
+          <Register/>
+        </Route>
+
+        <Route path = "/profile/:username">
+          <Profile/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App
